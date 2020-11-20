44001d53dd6f84fbc1960a030ebc5ef17adffdcc
Rujun Chen <rujche@microsoft.com>
2020-11-20 08:00:35 +0800

    Init project by spring initlizer.

---
+++ src/main/java/chenrujun/azure/spring/boot/starter/servicebus/jms/sample/AzureSpringBootStarterServicebusJmsSampleApplication.java
@@ -0,0 +1,13 @@
+package chenrujun.azure.spring.boot.starter.servicebus.jms.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSpringBootStarterServicebusJmsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSpringBootStarterServicebusJmsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chenrujun/azure/spring/boot/starter/servicebus/jms/sample/AzureSpringBootStarterServicebusJmsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package chenrujun.azure.spring.boot.starter.servicebus.jms.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSpringBootStarterServicebusJmsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
